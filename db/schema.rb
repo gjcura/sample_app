9bd99149f38d9bfc22392658e292ef8937762c46
Garrett <garrettcura@gmail.com>
2013-11-08 08:50:05 -0500

    Finish sign in

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20131108023702) do
-=======
-ActiveRecord::Schema.define(version: 20131107175858) do
->>>>>>> sign-in-out
 
   create_table "users", force: true do |t|
     t.string   "name"
